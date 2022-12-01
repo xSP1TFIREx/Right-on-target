bc4e41b486a017eec9c2ec7bfddfc7119c949774
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-12-01 15:03:08 +0300

    Initial commit

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Right on target
-//
-//  Created by Дмитрий on 01.12.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
